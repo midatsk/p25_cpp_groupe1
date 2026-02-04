9792bc385556c65afbf687d51068742d21b4c8b3
Michel Datskevitch <michel.datskevitch@etu.minesparis.psl.eu>
2026-02-04 17:37:50 +0100

    Premier TP stack

--- stack-tp.cpp
+++ stack-tp.cpp
@@ -1,154 +1,96 @@
 #include <iostream>
+#include <stdexcept>
 
-// Nous allons implémenter une pile d'entiers de manière très basique.
-// Nous la complexifierons par la suite.
-
-// La pile est constituée:
-//    - d'un tableau d'entier, avec sa taille et son nombre d'éléments empilés
-//    - des quatre fonctions: push, pop, top, print, is_empty et is_full
-//      (qui testent si la pile est resp. vide ou pleine)
-//      il faut les utiliser dans votre code de push et pop
-
-void print(int tab[], int size, int nb)
-{
-    for (int i = 0; i < nb; i++)
-    {
-        std::cout << tab[i] << "  " << std::endl;
-    }
-}
-
-// renvoie true si la pile est vide, false sinon
+// renvoie true si la pile est vide
 bool is_empty(int tab[], int size, int &nb)
 {
-    if (nb == 0)
-    {
-        return true;
-    }
-    return false;
+    return nb == 0;
 }
 
-// renvoie true si la pile est pleine, false sinon
+// renvoie true si la pile est pleine
 bool is_full(int tab[], int size, int &nb)
 {
-    if (nb == size)
+    return nb == size;
+}
+
+void print(int tab[], int size, int &nb)
+{
+    std::cout << "[ ";
+    for (int i = 0; i < nb; i++)
     {
-        return true;
+        std::cout << tab[i] << " ";
     }
-    return false;
+    std::cout << "]" << std::endl;
 }
 
 void push(int tab[], int size, int &nb, int elem)
 {
-    try
+    if (is_full(tab, size, nb))
     {
-        if (is_full(tab, size, nb) == false)
-        {
-            tab[nb] = elem;
-            nb++;
-        }
-        else
-        {
-            throw(is_full(tab, size, nb));
-        }
+        throw std::runtime_error("Pile pleine");
     }
-    catch (bool val)
-    {
-        std::cout << "Le tableau est plein" << std::endl;
-    }
+
+    tab[nb] = elem;
+    nb++;
 }
 
-int top(int tab[], int size, int &nb) // (cette fonction ne dépile pas)
-// cette fonction retourne l'entier en haut de la pile (le dernier empilé)
-// à la sortie de cette fonction l'élément retourné reste dans la pile
-// elle doit s'assurer que la pile n'est pas vide avant de la dépiler
-// elle lance une exception si problème
+int top(int tab[], int size, int &nb)
 {
-    try
+    if (is_empty(tab, size, nb))
     {
-        if (is_empty(tab, size, nb) == false)
-        {
-            return tab[nb];
-        }
-        else
-        {
-            throw(is_empty(tab, size, nb));
-        }
+        throw std::runtime_error("Pile vide");
     }
-    catch (bool val)
-    {
-        std::cout << "Le tableau est vide" << std::endl;
-    }
+
+    return tab[nb - 1];
 }
 
-int pop(int tab[], int size, int &nb) // (cette fonction dépile)
-// cette fonction retourne l'entier en haut de la pile (le dernier empilé)
-// à la sortie de cette fonction l'élément retourné n'est plus compté dans la pile
-// elle doit s'assurer que la pile n'est pas vide avant de la dépiler
-// elle lance une exception si problème
+int pop(int tab[], int size, int &nb)
 {
-
-    int tab_aux[size];
-    try
+    if (is_empty(tab, size, nb))
     {
-        if (is_empty(tab, size, nb) == false)
-        {
-            return tab[nb];
-        }
-        else{
-            throw(is_empty(tab,size,nb));
-        }
-    }
-    catch(bool val){
-        std::cout<<"Le tableau est vide"<<std::endl;
+        throw std::runtime_error("Pile vide");
     }
 
-    for (int i = 0; i < nb - 1; i++)
-    {
-        tab_aux[i] = tab[i];
-        int *ptab = tab;
-        int *ptab_aux = tab_aux;
-        ptab = ptab_aux;
-        nb = nb - 1;
-    }
+    nb--;
+    return tab[nb];
 }
 
-#include <stdexcept>
-    // https://en.cppreference.com/w/cpp/header/stdexcept.html
-    // pour un exemple d'exception voir le fichier exception.cpp
+int main()
+{
+    int size = 5;
+    int stack[size];
+    int nb = 0;
 
-    int main()
+    print(stack, size, nb);
+
+    push(stack, size, nb, -17);
+    std::cout << top(stack, size, nb) << std::endl;
+
+    push(stack, size, nb, 90);
+    print(stack, size, nb);
+
+    try
     {
-
-        // on définit les 3 variables constituant la pile
-        int size = 5;
-        int stack[size];
-        int nb = 0;
-
-        // rajoutez aux blocks catch les erreurs
-        // pouvant être déclenchées par les opérations sur la pile
-        try
-        {
-            print(stack, size, nb); // affiche [ [
-            push(stack, size, nb, -17);
-
-            // on affiche l'élément en haut de pile
-            std::cout << top(stack, size, nb) << std::endl; // affiche -17
-
-            print(stack, size, nb); // affiche [-17 90 [
-
-            int e = pop(stack, size, nb);
-            std::cout << e << std::endl;
-            print(stack, size, nb); // affiche [-17 [
-
-            push(stack, size, nb, 20);
-            push(stack, size, nb, -78);
-            push(stack, size, nb, -9);
-            push(stack, size, nb, -56);
-            push(stack, size, nb, -9);
-            push(stack, size, nb, 68);
-        }
-        catch (bool val)
-        {
-            std::cout<<"Le tableau est vide"<<std::endl;
-        }
+        int e = pop(stack, size, nb);
+        std::cout << e << std::endl;
     }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    try
+    {
+        push(stack, size, nb, 20);
+        push(stack, size, nb, -78);
+        push(stack, size, nb, -9);
+        push(stack, size, nb, -56);
+        push(stack, size, nb, 68); // déclenche exception
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    print(stack, size, nb);
+}
